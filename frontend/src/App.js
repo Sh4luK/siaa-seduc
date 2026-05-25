97b38621efebe385304f6b68770ab1aca07f7f9d
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-05-25 16:26:38 +0000

    feat(front/login): create login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route, createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Login />,
+//   },
+//   {
+//     path: "/home",
+//     element: <Home />,
+//   }
+// ]);
 
 function App() {
   return (
-    <div className="App">
-      <a className='text-danger'>Hello World</a>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- frontend/src/login/Login.js
+++
@@ -1,7 +0,0 @@
-export default function Login(){
-    return (
-        <div className="Login">
-            <a className='text-danger'>Hello</a>
-        </div>
-    )
-}
