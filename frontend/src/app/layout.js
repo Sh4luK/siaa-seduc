2026f2821720b4138c5896ea92bf1b20e4e15d81
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-05-29 00:47:29 +0000

    feat(front/header): create header

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -10,7 +10,20 @@ export default function RootLayout({ children }) {
       lang="pt-br"
       // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body>{children}</body>
+      <body>
+        <header className="p-3 mb-3 border-bottom">
+          <div className="container">
+            <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+              <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                <img src="/logo.png" alt="Logo" width={50} height={50} />
+              </a>
+              <span className="fs-4 ms-3">SIAA - Sistema Integrado de Acompanhamento Acadêmico</span>
+            </div>
+          </div>
+
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
