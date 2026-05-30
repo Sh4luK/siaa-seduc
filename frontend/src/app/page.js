d8879d1ef4b1eeed6b5c6a51497636ba280e6a43
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-05-30 14:09:16 +0000

    feat(frontend/ayout/tests): testing the login api in homepage frontend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,23 @@
+async function createJson(){
+  const data = {
+    "username": "admin",
+    "password": "admin123"
+  }
+  const url = "https://animated-parakeet-97456gj46g96fp4gp-8000.app.github.dev/login/"
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  const result = await response.json();
+  console.log(result);
+}
+
 export default function Home() {
+  createJson();
   return (
     <div>
       <h1>Hello World</h1>
