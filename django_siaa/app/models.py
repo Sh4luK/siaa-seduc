b60f7ec2e6efd9baeb50b4bc54bff58ce6d30fb1
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-05-30 17:44:36 +0000

    feat(models/user): rename column name to username

--- django_siaa/app/models.py
+++ django_siaa/app/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=250)
+    username = models.CharField(max_length=250)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=250)
 
     def __str__(self):
-        return self.name
+        return self.username
