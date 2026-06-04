201655b20f3fcbdbad6c34f1f336864336a4a8fd
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-04 01:20:28 +0000

    feat(frontend/login): refatorar lógica de autenticação e melhorar o tratamento de login

--- frontend/src/app/login/layout.js
+++ frontend/src/app/login/layout.js
@@ -1,49 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios"
 import { redirect } from "next/navigation"
+
+
 export const metadata = {
   title: "SIAA - Sistema Integrado de Acompanhamento Acadêmico",
   description: "Sistema de integração desenvolvido por alunos da rede de ensino da escola CETI Calisto Lobo, juntamente com a SEDUC-PI",
 };
 
 export default function RootLayout({ children }) {
-  const base_url = "https://animated-parakeet-97456gj46g96fp4gp-8000.app.github.dev"
-  const login_url = `${base_url}/api/login/`;
-  const auth_url = `${base_url}/api/auth/`;
-      
-  
-  
-  console.log("Checking authentication status...");
-  const res = axios.get(auth_url, {
+  // check authentication status
+  const authCheck = fetch("https://animated-parakeet-97456gj46g96fp4gp-8000.app.github.dev/api/auth/", {
       method: "GET",
-      headers: {
-          "Content-Type": "application/json"
+      credentials: "include"
+  });
+  authCheck.then(async(res) => {
+      const data = await res.json();
+      if(data.status === "success"){
+          console.log(`User ${data.user.username} is already authenticated, redirecting to dashboard.`);
+          return redirect("/dashboard");
+      }else{
+          console.log("User not authenticated, staying on login page.");
       }
   })
-  if (res.status === 200) {
-      console.log("User is authenticated");
-      redirect("/dashboard");
-  } else {
-      console.log("User is not authenticated, redirecting to login...");
-    }
-    return (
-      <html
-        lang="pt-br"
-        // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      >
-        <body>
-          {children}
-        </body>
-      </html>
-    )
-  // return (
-  //   <html
-  //     lang="pt-br"
-  //     // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-  //   >
-  //     <body>
-  //       {children}
-  //     </body>
-  //   </html>
-  // );
+  return (
+    <html
+      lang="pt-br"
+      // className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
