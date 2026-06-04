221da7cb0b28c115f6972582a166abaa38d33f3e
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-04 18:25:26 +0000

    feat(auth): refatorar lógica de autenticação no dashboard e layout de login

--- frontend/src/app/dashboard/page.js
+++ frontend/src/app/dashboard/page.js
@@ -1,4 +1,10 @@
-export default function  Dashboard() {
+import axios from "axios"
+
+export default async function  Dashboard() {
+    // fetch user data informations
+    const user = await fetch("https://animated-parakeet-97456gj46g96fp4gp-8000.app.github.dev/api/auth/")
+    const data = await user.json();
+    console.log(data)
     return (
         <div>
             <h1>Dashboard</h1>
--- frontend/src/app/login/layout.js
+++ frontend/src/app/login/layout.js
@@ -8,21 +8,22 @@ export const metadata = {
   description: "Sistema de integração desenvolvido por alunos da rede de ensino da escola CETI Calisto Lobo, juntamente com a SEDUC-PI",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
   // check authentication status
-  const authCheck = fetch("https://animated-parakeet-97456gj46g96fp4gp-8000.app.github.dev/api/auth/", {
-      method: "GET",
-      credentials: "include"
-  });
-  authCheck.then(async(res) => {
-      const data = await res.json();
-      if(data.status === "success"){
-          console.log(`User ${data.user.username} is already authenticated, redirecting to dashboard.`);
-          return redirect("/dashboard");
-      }else{
-          console.log("User not authenticated, staying on login page.");
-      }
-  })
+  const authCheck = await fetch("https://animated-parakeet-97456gj46g96fp4gp-8000.app.github.dev/api/auth/");
+  const auth = await authCheck.json();
+  console.log("Authentication check response:", auth);
+  
+
+  // authCheck.then(async(res) => {
+  //     const data = await res.json();
+  //     if(data.status === "success"){
+  //         console.log(`User ${data.user.username} is already authenticated, redirecting to dashboard.`);
+  //         return redirect("/dashboard");
+  //     }else{
+  //         console.log("User not authenticated, staying on login page.");
+  //     }
+  // })
   return (
     <html
       lang="pt-br"
