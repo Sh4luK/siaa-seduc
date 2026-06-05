fbc0750a7f5d75231e02212c1a9199fd9d135e80
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-05 11:42:52 +0000

    feat(ip): adicionar função para buscar o IP público

---
+++ django_siaa/app/funcs/ip.py
@@ -0,0 +1,9 @@
+import requests
+import os
+
+def fetchIp():
+    api = os.getenv("IP_API")
+    res = requests.get("https://api64.ipify.org/?format=json")
+    data = res.json()
+    print(data)
+    return data
