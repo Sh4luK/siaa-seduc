93cdcc7683f7f60b45ae0dc9117b7ec771579a12
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-05 11:17:40 +0000

    feat(models): adicionar campos adicionais ao modelo User

--- django_siaa/app/models.py
+++ django_siaa/app/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 class User(models.Model):
+    id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=250)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=250)
+    full_name = models.CharField(max_length=300, null=True)
+    registration = models.CharField(max_length=20, unique=True, null=True)
+    address = models.CharField(max_length=500, null=True)
+    phone_number = models.CharField(max_length=30, null=True)
+    is_active = models.BooleanField(default=True)
+    position = models.CharField(max_length=100, null=True)
+    cpf = models.CharField(max_length=14, unique=True, null=True)
+
+
 
     def __str__(self):
-        return self.username
+        return self.full_name
