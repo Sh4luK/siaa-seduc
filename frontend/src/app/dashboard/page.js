9d9e1a11269c1fff194b44da3a5290f9187be0fe
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-17 14:35:17 +0000

    feat(frontend/index): create frontend page

--- frontend/src/app/dashboard/page.js
+++
@@ -1,8 +0,0 @@
-export default async function  Dashboard() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <p>Welcome to the dashboard! You are successfully authenticated.</p>
-        </div>
-    );
-}
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,27 @@
 import axios from "axios";
 import { redirect } from "next/navigation"
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Image } from "next/image";
+import logo from "../assets/logo.png";
 
 export default function Home() {
   
   return (
     <div>
-      <h1>Home Page</h1>
+      <div className="d-flex justify-content-center align-items-center vh-100 shadow">
+        <div className="text-center">
+          <img src={logo.src} alt="Logo" className="mb-4" style={{ width: "200px", height: "auto" }} />
+          <h1 className="mb-4">Bem-vindo ao SIAA</h1>
+          <p className="mb-4">Sistema de Informação de Alunos e Aulas</p>
+          <small>Defina qual modulo deseja acessar</small>
+          <div className="d-grid gap-2">
+            <a className="btn btn-dark mx-2 mb-1" href="/aluno">Alunos</a>
+            <a className="btn btn-dark mx-2 mb-1" href="/responsavel">Responsavel</a>
+            <a className="btn btn-dark mx-2 mb-1" href="/professor">Professor</a>
+            <a className="btn btn-dark mx-2 mb-1" href="/coordenador">Coordenador</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
