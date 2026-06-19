07f0936619d8e163de265a4f8426beb0405f1548
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-19 00:18:49 +0000

    feat(layout): adicionar metadados com título da aplicação

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
+export const metadata = {
+  title: "Bem-vindo ao SIAA"
+} 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
