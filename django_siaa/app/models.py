c87e89d42c94355e2a83f06bdf33cfac2e4227ed
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-21 00:50:31 +0000

    feat(models): adicionar campo 'escola' aos modelos Professor, Estudante e Responsavel; criar modelo SuperAdmin

--- django_siaa/app/models.py
+++ django_siaa/app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Professor(models.Model):
     id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=300)
+    escola = models.CharField(max_length=170, null=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=30, null=True)
     address = models.CharField(max_length=500, null=True)
@@ -17,6 +18,7 @@ class Professor(models.Model):
 class Estudante(models.Model):
     id = models.AutoField(primary_key=True)
     matricula = models.CharField(max_length=20, unique=True)
+    escola = models.CharField(max_length=170, null=True)
     full_name = models.CharField(max_length=300)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=30, null=True)
@@ -47,6 +49,7 @@ class Responsavel(models.Model):
 class Coordenacao(models.Model):
     id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=300)
+    escola = models.CharField(max_length=170, null=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=30, null=True)
     address = models.CharField(max_length=500, null=True)
@@ -54,5 +57,15 @@ class Coordenacao(models.Model):
     is_active = models.BooleanField(default=True)
     cargo = models.CharField(max_length=150, null=True)
     ip = models.CharField(max_length=45, null=True)
+    def __str__(self):
+        return self.full_name
+
+class SuperAdmin(models.Model):
+    id = models.AutoField(primary_key=True)
+    full_name = models.CharField(max_length=300, unique=True)
+    email = models.EmailField(unique=True)
+    escola = models.CharField(max_length=170, null=True)
+    phone_number = models.CharField(max_length=30, null=True)
+    cpf = models.CharField(max_length=14, unique=True, null=True)
     def __str__(self):
         return self.full_name
