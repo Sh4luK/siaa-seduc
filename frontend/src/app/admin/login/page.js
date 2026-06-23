37f1203315b1d26af48e2be3eefd1445757cd4d8
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-23 11:48:57 +0000

    feat(admin): criar página de login para administração

---
+++ frontend/src/app/admin/login/page.js
@@ -0,0 +1,11 @@
+export default function AdminLoginPage(){
+    return (
+        <div className="d-flex justify-content-center align-items-center vh-100" style={{ backgroundColor: "#8cddaf" }}>
+            <div className="card shadow" style={{ borderRadius: "10px", padding: "10px" }}>
+                <form method="POST" className="form">
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
