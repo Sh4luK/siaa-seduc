492c99221e9de59060eed6c49a1267e9fbbf7744
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-27 01:29:42 +0000

    refact(infra/get_ip): remove unused get_ip function

--- infra/get_ip.py
+++
@@ -1,9 +0,0 @@
-def get_ip(request):
-
-    x_forwarder_for = request.META.get("HTTP_X_FORWARDER_FOR")
-    if x_forwarder_for:
-        ip = x_forwarder_for.split(",")[0].strip()
-    else:
-        ip = request.META.get("REMOTE_ADDR")
-    
-    return ip
