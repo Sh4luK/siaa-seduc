cae3747939b8a2954e6f0ccbfa407aa95b59cb51
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-27 02:39:46 +0000

    feat(migrations): alter Estudante model fields for IP and senha

--- django_siaa/django_siaa/urls.py
+++ django_siaa/django_siaa/urls.py
@@ -4,10 +4,8 @@ import app.views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", app.views.index, name="index"),
-    path("api/login/", app.views.login_api, name="login"),
-    path("api/register/", app.views.register_api, name="register"),
-    # path("adm/login/", app.views.AdminApiLoginSuperUser, name="admLogin"),
     path("api/students", app.views.view_students, name="students"),
-    path('api/students/search', app.views.search_student, name='general_student_search')
-    # path("api/auth/", app.views.protected_api, name="protected"),
+    path('api/students/search', app.views.search_student, name='general_student_search'),
+    path("api/students/login", app.views.login_student, name="login_student"),
+    path("api/students/auth", app.views.auth_student, name="auth_student"),
 ]
