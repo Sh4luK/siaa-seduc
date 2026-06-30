2af4fb1a04d8fe4e4313c076c0ca2e8693b36323
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-06-30 01:05:43 +0000

    feat: me perdi nos commits(acho que tava fazendo o cabeçalho do modulo aluno

--- frontend/src/app/aluno/page.js
+++ frontend/src/app/aluno/page.js
@@ -8,8 +8,14 @@ import axios from "axios"
 export default function alunoPage(){
     const [authenticated, setAuthenticated] = useState(null)
     const [loading, setLoading] = useState(true)
-    const [studentFullName, setStudentFullName] = useState("Carregando...")
+    const [nomeCompleto, setNomeCompleto] = useState("")
+    const [periodo, setPeriodo] = useState("")
+    const [escola, setEscola] = useState("")
+    const [serie, setSerie] = useState("")
+    const [turma, setTurma] = useState("")
+    const [primeiroNome, setPrimeiroNome] = ("")
     const router = useRouter()
+
     useEffect(()=>{
         async function verifyAuthentication(){
             try {
@@ -38,9 +44,13 @@ export default function alunoPage(){
                 if(!res.ok) throw new Error()
                 return res.json()
             }).then((data)=>{
-                setStudentFullName(data["student"]["nome_completo"] || "Não encontrado.")
+                setNomeCompleto(data["student"]["nome_completo"] || "Não encontrado.")
+                setPeriodo(data["student"]["periodo"])
+                setEscola(data["student"]["escola"])
+                setSerie(data["student"]["serie"])
+                setTurma(data["student"]["turma"])
             }).catch((error)=>{
-                setStudentFullName("Erro ao carregar.")
+                setNomeCompleto("Erro ao carregar.")
             })
         }
         getStudent()
@@ -59,9 +69,104 @@ export default function alunoPage(){
     }
 
     if(authenticated === true){
+        const firstName = nomeCompleto.split(" ")[0]
         return (
-            <div className="d-flex align-items-center justify-content-center vh-100">
-                {studentFullName}
+            <div className="">
+            { /*
+                <header className="d-flex justify-content-between p-2 border-bottom shadow">
+                    <div className="d-flex">
+                        <a className="d-flex align-items-center mb-3 mb-md-0 me-md-auto me-2 text-decoration-none mr-3">
+                            <img src={logo.src} style={{ width: "100px", height: "auto" }} />
+                            <span className="fs-4 text-dark">SIAA</span>
+                        </a>
+                        <ul className="nav nav-pills m-auto ms-3">
+                            <li className="nav-item">
+                                <a href="/" className="nav-link active">Painel</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link">Conteudo</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link">Atividades</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link">Boletim</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link">Cronograma</a>
+                            </li>
+                            <li className="nav-item">
+                                <a href="/" className="nav-link">Horario</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <div className="text-end">
+                        <span className="text-muted">
+                            <small className="nav nav-pills flex-column">
+                                {nomeCompleto}
+                                <small className="nav nav-pills flex-column">
+                                    {escola}
+                                    <small>
+                                        {turma}
+                                    </small>
+                                </small>
+                            </small>
+
+                        </span>
+                    </div>
+                </header>
+                */ }
+                
+                <header className="p-3 mb-3 border-bottom">
+                    <div className="container">
+                        <div className="d-flex flex-wrap align-items-center justify-content-center justifi-content-lg-start">
+                            <a className="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+                                <img src={logo.src} style={{ width: "100px", height: "auto" }} />
+                            </a>
+                            <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                                <li className="px-1">
+                                    <a className="btn btn-dark btn-sm" href="/aluno">Inicio</a>
+                                </li>
+                                <li className="px-1">
+                                    <a className="btn btn-outline-dark btn-sm" href="/aluno/conteudos">Conteudos</a>
+                                </li>
+                                <li className="px-1">
+                                    <a className="btn btn-outline-dark btn-sm" href="/aluno/atividades">Atividades</a>
+                                </li>
+                                <li className="px-1">
+                                    <a className="btn btn-outline-dark btn-sm" href="/aluno/boletim">Boletim</a>
+                                </li>
+                                <li className="px-1">
+                                    <a className="btn btn-outline-dark btn-sm" href="/aluno/cronograma">Cronograma</a>
+                                </li>
+                                <li className="px-1">
+                                    <a className="btn btn-outline-dark btn-sm" href="/aluno/horarios">Horarios</a>
+                                </li>
+                            </ul>
+                            <div className="text-end p-3 text-bg-success rounded shadow">
+                                <small>{nomeCompleto}</small>
+                                <br />
+                                <small>{turma}</small>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                
+                
+                <br /><br />
+                <main className="container">
+                    <h1 className="fs-4">Olá, {firstName}</h1>
+                    <small className="text-small">Bem-vindo ao Sistema Integrado de Acompanhamento Academico</small>
+
+                    <section className="container marketing">
+                        <div className="row">
+                            <div className="col-md-4">
+                                
+                            </div>
+                        </div>
+                    </section>
+                </main>
             </div>
         );
     }
--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" data-scroll-behavior="smooth">
       <body>
         {children}
       </body>
