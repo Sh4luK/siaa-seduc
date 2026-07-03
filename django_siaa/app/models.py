1fceaa1cb2c18549df284b273be944a94abbdbc5
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-07-03 01:27:02 +0000

    feat(models): relational carga

--- django_siaa/app/models.py
+++ django_siaa/app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Professor(models.Model):
-    id = models.AutoField(primary_key=True)
+    """id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=300)
     escola = models.CharField(max_length=170, null=True)
     email = models.EmailField(unique=True)
@@ -11,24 +11,30 @@ class Professor(models.Model):
     is_active = models.BooleanField(default=True)
     materia = models.CharField(max_length=700, null=True)
     classes = models.CharField(max_length=700, null=True)
-    ip = models.CharField(max_length=45, null=True)
+    ip = models.CharField(max_length=45, null=True)"""
+    nome_completo = models.CharField(max_length=255, unique=True, null=True)
+    data_cadastro = models.DateTimeField(auto_now_add=True, null=True)
+    senha = models.CharField(max_length=150, null=True)
     def __str__(self):
-        return self.full_name
+        return self.nome_completo
+
+class Disciplina(models.Model):
+    professores = models.ManyToManyField(Professor, related_name="disciplinas")
+    nome_disciplina = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.nome_disciplina
+
+class AtravessaPor(models.Model):
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name="atravessamentos")
+    escola = models.CharField(max_length=255, null=True)
+    turma = models.CharField(max_length=255, null=True)
+    etapa = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return f"{self.professor.nome_completo} -> {self.turma}"
 
 class Estudante(models.Model):
-    # id = models.AutoField(primary_key=True)
-    # matricula = models.CharField(max_length=20, unique=True)
-    # escola = models.CharField(max_length=170, null=True)
-    # full_name = models.CharField(max_length=300)
-    # email = models.EmailField(unique=True)
-    # phone_number = models.CharField(max_length=30, null=True)
-    # address = models.CharField(max_length=500, null=True)
-    # cpf = models.CharField(max_length=14, unique=True, null=True)
-    # is_active = models.BooleanField(default=True)
-    # mom_name = models.CharField(max_length=300, null=True)
-    # course = models.CharField(max_length=170, null=True)
-    # ip = models.CharField(max_length=45, null=True)
-
     posicao_ordem = models.IntegerField(null=True)
     nome_completo = models.CharField(max_length=255, null=True)
     escola = models.CharField(max_length=255, null=True)
@@ -71,6 +77,7 @@ class Coordenacao(models.Model):
     def __str__(self):
         return self.full_name
 
+"""
 class SuperAdmin(models.Model):
     id = models.AutoField(primary_key=True)
     full_name = models.CharField(max_length=300, unique=True)
@@ -82,3 +89,4 @@ class SuperAdmin(models.Model):
     ip = models.CharField(max_length=20, null=True)
     def __str__(self):
         return self.full_name
+"""
