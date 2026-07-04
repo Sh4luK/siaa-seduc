708605dd4db22a775456ffce80e576e9137e43ba
iiraildess <joseiraildesciprianoribeiro2@gmail.com>
2026-07-04 01:41:22 +0000

    feat(django/url): define new route for login student

--- django_siaa/django_siaa/urls.py
+++ django_siaa/django_siaa/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api/students/search', app.views.search_student, name='general_student_search'),
     path("api/students/login", app.views.login_student, name="login_student"),
     path("api/students/auth", app.views.auth_student, name="auth_student"),
+    path("api/teacher/login", app.views.login_teacher, name="login_teacher")
 ]
