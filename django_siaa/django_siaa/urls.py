ff562fa62f1db62f52cc1c5b350ebf936b8348da
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-07-20 01:49:18 +0000

    refactor(back/views): uncommiting

--- django_siaa/django_siaa/urls.py
+++ django_siaa/django_siaa/urls.py
@@ -22,23 +22,3 @@ urlpatterns = [
     path("api/students/notas", app.views.get_boletim_aluno, name="get_boletim_aluno"),
     path("api/teacher/turma/disciplinas", app.views.get_disciplinas_da_turma, name="get_disciplinas_da_turma"),
 ]
-# urlpatterns = [
-#     path("admin/", admin.site.urls),
-#     path("", app.views.index, name="index"),
-#     path("api/students", app.views.view_students, name="students"),
-#     path('api/students/search', app.views.search_student, name='general_student_search'),
-#     path("api/students/login", app.views.login_student, name="login_student"),
-#     path("api/students/auth", app.views.auth_student, name="auth_student"),
-#     path("api/teacher/login", app.views.login_teacher, name="login_teacher"),
-#     path("api/teacher/auth", app.views.auth_teacher, name="authentication_teacher"),
-#     path("api/teacher/search", app.views.search_teacher, name="search_teacher"),
-#     path("api/teacher/search/turmas", app.views.get_turmas, name="get_turmas"),
-#     path("api/teacher/search/turma", app.views.get_turma, name="get_turma"),
-#     path("api/teacher/get/alunos", app.views.get_alunos_por_turma, name="get_alunos_por_turma"),
-#     path("api/teacher/search/disciplinas", app.views.get_disciplinas_lecionadas, name="get_disciplinas"),
-#     path("api/teacher/notas/get", app.views.get_notas_aluno, name="get_notas_aluno"),
-#     path("api/teacher/notas/salvar", app.views.salvar_notas, name="salvar_notas"),
-#     path("api/teacher/notas/turma/get", app.views.get_notas_turma, name="get_notas_turma"),
-#     path("api/teacher/notas/turma/salvar", app.views.salvar_notas_turma, name="salvar_notas_turma"),
-#     path("api/students/notas", app.views.get_boletim_aluno, name="get_boletim_aluno"),
-# ]
