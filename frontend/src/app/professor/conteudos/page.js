6328d8a1bfda4db9bbb7247caba26b6af8b7aba9
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-08 13:31:25 +0000

    feat(conteudos): add link to ComunicadosPage in navigation

--- frontend/src/app/professor/conteudos/page.js
+++ frontend/src/app/professor/conteudos/page.js
@@ -534,6 +534,10 @@ export default function ConteudosPage() {
               <i className="ti ti-users" aria-hidden="true" />
               Conteudos
             </Link>
+            <Link href="/professor/comunicados" className={layoutStyles.navLink}>
+              <i className="ti ti-message" aria-hidden="true" />
+              Comunicados
+            </Link>
             <Link href="/professor/atividades" className={layoutStyles.navLink}>
               <i className="ti ti-users" aria-hidden="true" />
               Atividades
