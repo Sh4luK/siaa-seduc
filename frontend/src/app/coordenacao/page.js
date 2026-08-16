da0e4aa91b5d254cafc794495393efb24965990b
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-16 14:16:36 +0000

    chore(coordenacao): atualizar URL da API na pagina principal da coordenacao

--- frontend/src/app/coordenacao/page.js
+++ frontend/src/app/coordenacao/page.js
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
-const API_BASE = "https://q0w7c17l-8000.brs.devtunnels.ms";
+const API_BASE = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev";
 
 const SECOES = [
   {
