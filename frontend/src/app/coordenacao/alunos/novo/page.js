01b380d2f746d147e2ca261249f98f7b09c4a8ff
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-16 14:16:36 +0000

    chore(coordenacao): atualizar URL da API em alunos/novo

--- frontend/src/app/coordenacao/alunos/novo/page.js
+++ frontend/src/app/coordenacao/alunos/novo/page.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
-const API_BASE = "https://q0w7c17l-8000.brs.devtunnels.ms";
+const API_BASE = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev";
 
 export default function NovoAlunoPage() {
   const router = useRouter();
