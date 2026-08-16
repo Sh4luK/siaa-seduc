b566cc5d901b1fb84bac0c0ff9fa81a1e75606ef
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-16 00:36:13 +0000

    fix(api): update API_BASE URL to the correct server

--- frontend/src/app/professor/page.js
+++ frontend/src/app/professor/page.js
@@ -7,7 +7,7 @@
 // import Link from "next/link"
 // import styles from "./page.module.css"
 
-// const API_BASE = "https://upgraded-space-spork-4j9vqpw9q5g5fprr-8000.app.github.dev";
+// const API_BASE = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev";
 
 // export default function Professor() {
 //   const [authenticated, setAuthenticated] = useState(null)
@@ -305,7 +305,7 @@ import Link from "next/link"
 import layoutStyles from "./page.module.css"
 import styles from "./home.module.css"
 
-const API_BASE = "https://upgraded-space-spork-4j9vqpw9q5g5fprr-8000.app.github.dev";
+const API_BASE = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev";
 
 export default function Professor() {
   const [authenticated, setAuthenticated] = useState(null)
@@ -614,7 +614,7 @@ export default function Professor() {
 // import layoutStyles from "./page.module.css"
 // import styles from "./home.module.css"
 
-// const API_BASE = "https://upgraded-space-spork-4j9vqpw9q5g5fprr-8000.app.github.dev";
+// const API_BASE = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev";
 
 // function IconCalendario() {
 //   return (
