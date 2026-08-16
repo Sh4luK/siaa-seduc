245921ee697d5e7583cce23d96039dbd63791a97
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-16 14:16:36 +0000

    chore(coordenacao): atualizar URL da API em alunos

--- frontend/src/app/coordenacao/alunos/page.js
+++ frontend/src/app/coordenacao/alunos/page.js
@@ -7,7 +7,7 @@ import styles from "./page.module.css";
 import logo from "../../../assets/logo.png";
 import Image from "next/image";
 
-const API_BASE = "https://q0w7c17l-8000.brs.devtunnels.ms";
+const API_BASE = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev";
 
 export default function AlunosCoordenacaoPage() {
   const [authenticated, setAuthenticated] = useState(null);
