47d1e71ce0765fb76b9a4edb775412b38f1bae05
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-16 00:31:06 +0000

    fix(api): update API_BASE URL to correct endpoint

--- frontend/src/app/coordenacao/alunos/[alunoId]/page.js
+++ frontend/src/app/coordenacao/alunos/[alunoId]/page.js
@@ -5,7 +5,7 @@ import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
-const API_BASE = "https://upgraded-space-spork-4j9vqpw9q5g5fprr-8000.app.github.dev";
+const API_BASE = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev";
 
 function formatarData(dataISO) {
   const [ano, mes, dia] = dataISO.split("-");
