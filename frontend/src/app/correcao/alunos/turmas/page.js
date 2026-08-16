6ea17f7ad7804645cf21f244f84776dd71485de2
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-16 14:16:37 +0000

    chore(correcao): atualizar URL da API em alunos/turmas

--- frontend/src/app/correcao/alunos/turmas/page.js
+++ frontend/src/app/correcao/alunos/turmas/page.js
@@ -12,7 +12,7 @@ export default function CorrigirAlunos() {
   useEffect(() => {
     async function allStudents() {
       const response = await fetch(
-        "https://q0w7c17l-8000.brs.devtunnels.ms/api/students/search",
+        "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev/api/students/search",
       );
       if (!response.ok) {
         throw new Error();
