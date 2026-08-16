8623d71f61c5128ce3a57028ebe3a0799ae4dcd2
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-16 14:16:38 +0000

    chore(professor): atualizar URL da API em turmas/[turmaId]/alunos/[alunoId]

--- frontend/src/app/professor/turmas/[turmaId]/alunos/[alunoId]/page.js
+++ frontend/src/app/professor/turmas/[turmaId]/alunos/[alunoId]/page.js
@@ -5,7 +5,7 @@ import { useParams, useRouter } from "next/navigation";
 import layoutStyles from "../../../../page.module.css";
 import styles from "./notas.module.css";
 
-const API_BASE = "https://q0w7c17l-8000.brs.devtunnels.ms";
+const API_BASE = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev";
 
 const RF_OPCOES = [
   ["CUR", "Cursando"], ["AP", "Aprovado"], ["RE", "Reprovado"],
