42b55e2fc90982d08fa60c397ca2591503a81b19
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-19 22:01:28 +0000

    feat: add endpoint to edit Comunicado for coordenacao

--- django_siaa/django_siaa/urls.py
+++ django_siaa/django_siaa/urls.py
@@ -66,7 +66,5 @@ urlpatterns = [
     path('api/coordenacao/comunicados', app.views.get_comunicados_coordenacao, name='get_comunicados_coordenacao'),
     path('api/coordenacao/comunicados/criar', app.views.criar_comunicado_coordenacao, name='criar_comunicado_coordenacao'),
     path('api/coordenacao/comunicados/<int:comunicado_id>', app.views.get_comunicado_detalhe, name='get_comunicado_detalhe'),
-
-
-
+    path('api/coordenacao/comunicados/<int:comunicado_id>/editar', app.views.editar_comunicado_coordenacao, name='editar_comunicado_coordenacao'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
