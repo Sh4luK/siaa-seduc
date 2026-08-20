ce0b73e454b7ca505e154afbfc3baf2824182c7c
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-08-20 21:32:32 +0000

    fix: update authentication API URL to new endpoint

--- frontend/src/app/aluno/page.js
+++ frontend/src/app/aluno/page.js
@@ -22,7 +22,7 @@ export default function alunoPage() {
     useEffect(() => {
         async function verifyAuthentication() {
             try {
-                const url = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev/api/students/auth"
+                const url = "https://obscure-happiness-v67rpjq7p96vfxj4g-8000.app.github.dev/api/students/auth"
                 const response = await fetch(url)
                 const data = await response.json()
                 console.log(data)
@@ -40,7 +40,7 @@ export default function alunoPage() {
             }
         }
         async function getStudent() {
-            const url = "https://cuddly-yodel-5gprv7xpvp7rf755x-8000.app.github.dev/api/students/auth"
+            const url = "https://obscure-happiness-v67rpjq7p96vfxj4g-8000.app.github.dev/api/students/auth"
 
             fetch(url)
                 .then((res) => {
