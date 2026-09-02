b4d2c111865e5ec5c8b597291de02c03948a8ae3
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-09-02 00:00:57 +0000

    feat: atualizar a URL da API para o ambiente de desenvolvimento

--- frontend/src/app/blog/novo/page.js
+++ frontend/src/app/blog/novo/page.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 
-const API_BASE = "https://humble-spoon-4j654556jr9vf5qp6-8000.app.github.dev";
+const API_BASE = "https://obscure-happiness-v67rpjq7p96vfxj4g-8000.app.github.dev";
 
 export default function NovoPostPage() {
   const router = useRouter();
