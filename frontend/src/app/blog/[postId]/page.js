f7fa548fb6420cb7449ba6cf4cdd93c2e3405106
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-09-02 00:00:28 +0000

    feat: atualizar a URL da API para o ambiente de desenvolvimento

--- frontend/src/app/blog/[postId]/page.js
+++ frontend/src/app/blog/[postId]/page.js
@@ -5,7 +5,7 @@ import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
-const API_BASE = "https://humble-spoon-4j654556jr9vf5qp6-8000.app.github.dev";
+const API_BASE = "https://obscure-happiness-v67rpjq7p96vfxj4g-8000.app.github.dev";
 
 function formatarData(dataISO) {
   const data = new Date(dataISO);
