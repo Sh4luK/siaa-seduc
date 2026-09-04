fdbe8f604e756ab813374546b1b06ec0be7a8246
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-09-04 00:18:56 +0000

    feat: adicionar classe de estilo ao link de frequência na página de atividades do aluno

--- frontend/src/app/aluno/atividades/page.js
+++ frontend/src/app/aluno/atividades/page.js
@@ -93,7 +93,7 @@ export default function AtividadesAlunoPage() {
               <i className="ti ti-clipboard-list" aria-hidden="true" />
               Atividades
             </Link>
-            <Link href="/aluno/frequencia">
+            <Link href="/aluno/frequencia" className={layoutStyles.navLink}>
               <i className="ti ti-calendar-stats" aria-hidden="true" />
               Frequência
             </Link>
