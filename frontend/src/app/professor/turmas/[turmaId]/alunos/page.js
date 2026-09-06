d0c61b3826794a40f46cf2a3522dc120d3d9cbb8
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-09-06 19:12:29 +0000

    feat: atualizar link de navegação para a página de mensagens, alterando o texto e o destino

--- frontend/src/app/professor/turmas/[turmaId]/alunos/page.js
+++ frontend/src/app/professor/turmas/[turmaId]/alunos/page.js
@@ -487,9 +487,9 @@ export default function AlunosDaTurmaPage() {
               <i className="ti ti-clipboard-check" aria-hidden="true" />
               Frequência
             </Link>
-            <Link href="/professor/horarios" className={layoutStyles.navLink}>
+            <Link href="/professor/mensagem" className={layoutStyles.navLink}>
               <i className="ti ti-clock" aria-hidden="true" />
-              Horários
+              Mensagens
             </Link>
           </nav>
 
