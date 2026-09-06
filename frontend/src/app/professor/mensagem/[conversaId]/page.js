ec76f7b869111e545dba19516a852a5f36408f65
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-09-06 19:11:49 +0000

    feat: atualizar link de navegação para a página de mensagens, alterando o texto e removendo link duplicado

--- frontend/src/app/professor/mensagem/[conversaId]/page.js
+++ frontend/src/app/professor/mensagem/[conversaId]/page.js
@@ -383,10 +383,6 @@ export default function ConversaProfessorPage() {
               <i className="ti ti-message" aria-hidden="true" />
               Comunicados
             </Link>
-            <Link href="/professor/mensagem" className={layoutStyles.navLinkActive}>
-              <i className="ti ti-messages" aria-hidden="true" />
-              Mensagens
-            </Link>
             <Link href="/professor/atividades" className={layoutStyles.navLink}>
               <i className="ti ti-users" aria-hidden="true" />
               Atividades
@@ -399,9 +395,9 @@ export default function ConversaProfessorPage() {
               <i className="ti ti-edit" aria-hidden="true" />
               Lançar notas
             </Link>
-            <Link href="/professor/horarios" className={layoutStyles.navLink}>
+            <Link href="/professor/mensagem" className={layoutStyles.navLink}>
               <i className="ti ti-clock" aria-hidden="true" />
-              Horários
+              Mensagens
             </Link>
           </nav>
 
