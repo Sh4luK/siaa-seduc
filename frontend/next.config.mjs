d3ad916d7c52e6e8d7e0e9a16362814da7cc7f72
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-09-13 21:03:07 +0000

    feat: adicionar configuração para turbopack no Next.js

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  turbopack: {
+    root: __dirname,
+  },
   reactCompiler: true,
   allowedDevOrigins: ['172.0.0.1'],
 };
