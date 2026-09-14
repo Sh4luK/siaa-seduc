63eef2326a253426c4cd432b1c6b8e8d38ce4261
José Cipriano <joseiraildesciprianoribeiro2@gmail.com>
2026-09-14 23:55:12 +0000

    feat: adicionar link para gerenciamento de horários na página de atividades: frontend/src/app/professor/mensagem/page.js

--- frontend/src/app/professor/mensagem/page.js
+++ frontend/src/app/professor/mensagem/page.js
@@ -152,8 +152,13 @@ export default function MensagensProfessorPage() {
             <Link href="/professor/notas" className={layoutStyles.navLink}>
               <i className="ti ti-edit" aria-hidden="true" />Lançar notas
             </Link>
-            <Link href="/professor/mensagem" className={layoutStyles.navLinkActive}>
-              <i className="ti ti-messages" aria-hidden="true" />Mensagens
+            <Link href="/professor/horarios" className={layoutStyles.navLink}>
+              <i className="ti ti-clock" aria-hidden="true" />
+              Horários
+            </Link>
+            <Link href="/professor/mensagem" className={layoutStyles.navLink}>
+              <i className="ti ti-clock" aria-hidden="true" />
+              Mensagens
             </Link>
           </nav>
 
